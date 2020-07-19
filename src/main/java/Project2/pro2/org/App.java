6ecce721f3ab2ce5fef0851df114282b53374b12
sambathsain <sambathsain2gmail.com>
2020-07-19 16:38:00 +0530

    commit

---
+++ src/main/java/Project2/pro2/org/App.java
@@ -0,0 +1,13 @@
+package Project2.pro2.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
